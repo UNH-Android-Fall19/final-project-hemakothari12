55d8a659439bb519649a23d37965a5a599c9db21
hemakothari12 <hkoth2@unh.newhaven.edu>
2019-10-10 16:45:20 -0400

    Admin Home Screen displayed list of users from database

--- app/src/main/java/com/example/sugarbroker/AdminHomeActivity.kt
+++ app/src/main/java/com/example/sugarbroker/AdminHomeActivity.kt
@@ -3,16 +3,25 @@ package com.example.sugarbroker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_admin_home.*
+import kotlinx.android.synthetic.main.activity_user_row.view.*
 
 class AdminHomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
+
+        fetchUser()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -30,8 +39,7 @@ class AdminHomeActivity : AppCompatActivity() {
                 return true
             }
             R.id.logout -> {
-                //Implement Logout
-                Toast.makeText(this, "Implement Logout", Toast.LENGTH_SHORT).show()
+                //Implemented Logout
                 performLogout()
                 return true
             }
@@ -46,4 +54,34 @@ class AdminHomeActivity : AppCompatActivity() {
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
     }
+
+    private fun fetchUser() {
+        val adapter = GroupAdapter<GroupieViewHolder>()
+
+        val documentRef = FirebaseFirestore.getInstance().collection("users")
+        documentRef.get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("Document Fetched", "${document.id} => ${document.data}")
+                    var user = User(document.getString("uid")!!, document.getString("name")!!, document.getString("address")!!, document.getString("phone")!!, document.getString("type")!!)
+                    adapter.add(UserItem(user))
+                }
+
+                userlist_recyclerview.adapter = adapter
+            }
+            .addOnFailureListener { exception ->
+                Log.d("Document Failed to fetch", "Error getting documents: ", exception)
+            }
+    }
 }
+
+class UserItem(val user: User): Item<GroupieViewHolder>() {
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.itemView.name_textview.text = user.name
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.activity_user_row
+    }
+}
+
