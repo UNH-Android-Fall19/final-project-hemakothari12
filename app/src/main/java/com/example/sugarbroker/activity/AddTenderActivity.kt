55b3fadf478488d25788d6776d72c6aa4509f4e3
hemakothari12 <hkoth2@unh.newhaven.edu>
2019-10-25 19:06:12 -0400

    Camera added

--- app/src/main/java/com/example/sugarbroker/activity/AddTenderActivity.kt
+++ app/src/main/java/com/example/sugarbroker/activity/AddTenderActivity.kt
@@ -1,13 +1,22 @@
 package com.example.sugarbroker.activity
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.sugarbroker.R
 import com.example.sugarbroker.model.Tender
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_add_tender.*
+import com.mikelau.croperino.CroperinoConfig
+import com.mikelau.croperino.Croperino
+import com.mikelau.croperino.CroperinoFileUtil
 
 class AddTenderActivity : AppCompatActivity() {
 
@@ -31,6 +40,20 @@ class AddTenderActivity : AppCompatActivity() {
             price_edittext.setText(bundle.getString("UpdateTenderPrice"))
         }
 
+        prepareCroperino()
+        if (title.isNotEmpty()) {
+            if (id!!.isNotEmpty()) {
+                add_button.text = "Update"
+            } else {
+                add_button.text = "Add"
+            }
+        }
+
+        sugar_image.setOnClickListener {
+            System.out.println("camera in use")
+            addSugarButtonClicked()
+        }
+
         add_button.setOnClickListener {
             val millName = mill_name_edittext.text.toString()
             val price = price_edittext.text.toString()
@@ -42,6 +65,7 @@ class AddTenderActivity : AppCompatActivity() {
                     addTender(millName, price)
                 }
             }
+
 
             finish()
 
@@ -78,4 +102,69 @@ class AddTenderActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Tender could not be added!", Toast.LENGTH_SHORT).show()
             }
     }
+
+    private fun prepareCroperino() {
+        //prepare camera, gallery and ask for storage permissions.
+        CroperinoConfig(
+            "IMG_" + System.currentTimeMillis() + ".jpg",
+            "/VProperty/Pictures",
+            "/sdcard/VProperty/Pictures"
+        )
+        CroperinoFileUtil.verifyStoragePermissions(this@AddTenderActivity)
+        CroperinoFileUtil.setupDirectory(this@AddTenderActivity)
+
+    }
+
+    private fun addSugarButtonClicked() {
+        Log.d("Camera in use", "Camera in use")
+        Croperino.prepareChooser(
+            this@AddTenderActivity,
+            "" + resources.getString(R.string.capture_photo),
+            ContextCompat.getColor(this@AddTenderActivity, android.R.color.background_dark)
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            CroperinoConfig.REQUEST_TAKE_PHOTO ->
+                if (resultCode == Activity.RESULT_OK) {
+                    /* Parameters of runCropImage = File, Activity Context, Image is Scalable or Not, Aspect Ratio X, Aspect Ratio Y, Button Bar Color, Background Color */
+                    Croperino.runCropImage(
+                        CroperinoFileUtil.getTempFile(),
+                        this@AddTenderActivity,
+                        true,
+                        1,
+                        1,
+                        R.color.gray,
+                        R.color.gray_variant
+                    )
+                }
+            CroperinoConfig.REQUEST_PICK_FILE ->
+                if (resultCode == Activity.RESULT_OK) {
+                    CroperinoFileUtil.newGalleryFile(data, this@AddTenderActivity)
+                    Croperino.runCropImage(
+                        CroperinoFileUtil.getTempFile(),
+                        this@AddTenderActivity,
+                        true,
+                        0,
+                        0,
+                        R.color.gray,
+                        R.color.gray_variant
+                    )
+                }
+            CroperinoConfig.REQUEST_CROP_PHOTO ->
+                if (resultCode == Activity.RESULT_OK) {
+                    //cropped image returned is set to the imageview on  register layout
+                    var i = Uri.fromFile(CroperinoFileUtil.getTempFile())
+                    Glide.with(this@AddTenderActivity).load(i)
+                        .placeholder(R.drawable.photoplaceholder)
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(
+                            sugar_image
+                        )
+                }
+        }
+    }
+
 }
