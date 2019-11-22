1ceec1a6e200554cde612737b35c97e5bbd552c1
hemakothari12 <hkoth2@unh.newhaven.edu>
2019-11-22 11:53:22 -0500

    Detail Users UI

--- app/src/main/java/com/example/sugarbroker/activity/users/DetailUserActivity.kt
+++ app/src/main/java/com/example/sugarbroker/activity/users/DetailUserActivity.kt
@@ -2,6 +2,7 @@ package com.example.sugarbroker.activity.users
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.sugarbroker.R
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_detail_user.*
@@ -19,6 +20,8 @@ class DetailUserActivity : AppCompatActivity() {
 
         firestoreDB = FirebaseFirestore.getInstance()
 
+        setUpToolbar()
+
         val bundle = intent.extras
         if (bundle != null) {
             id = bundle.getString("UpdateUserId")
@@ -29,7 +32,24 @@ class DetailUserActivity : AppCompatActivity() {
             address_textview.setText(bundle.getString("UpdateUserAddress"))
             phone_textview.setText(bundle.getString("UpdateUserPhone"))
             type_textview.setText(bundle.getString("UpdateUserType"))
+            tvIcon.setText(bundle.getString("UpdateUserName")!!.get(0).toUpperCase().toString())
         }
+    }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar!!.elevation = 4.0F
+        actionBar.setDisplayShowHomeEnabled(true)
+        actionBar.setDisplayUseLogoEnabled(true)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        val count = supportFragmentManager.backStackEntryCount
+        toolbar.setNavigationOnClickListener(View.OnClickListener {
+            if (count == 0)
+                super.onBackPressed()
+            else
+                supportFragmentManager.popBackStack()
+        })
     }
 
 }
