2ffe35fe301cf6d092efa821e0d912cd593b17e2
hemakothari12 <hkoth2@unh.newhaven.edu>
2019-11-27 17:08:01 -0500

    Splash screen and asnimation

--- app/src/main/java/com/example/sugarbroker/activity/MainActivity.kt
+++ app/src/main/java/com/example/sugarbroker/activity/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.sugarbroker.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 import com.example.sugarbroker.R
 import com.example.sugarbroker.activity.account.LoginActivity
@@ -12,6 +16,7 @@ import com.example.sugarbroker.activity.home.SellerHomeActivity
 import com.example.sugarbroker.activity.home.UserHomeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +24,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        adminName.visibility = View.GONE
 
         auth = FirebaseAuth.getInstance()
 
+        Handler().postDelayed({
+            adminName.visibility = View.VISIBLE
+        },4000)
+
+        Handler().postDelayed({
+            callHomePage()
+        },5000)
+    }
+
+    private fun callHomePage() {
         if (auth.currentUser != null) {
             val db = FirebaseFirestore.getInstance()
             val uid = FirebaseAuth.getInstance().uid ?: ""
@@ -38,6 +56,7 @@ class MainActivity : AppCompatActivity() {
                             intent = Intent(applicationContext, AdminHomeActivity::class.java)
                             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                             startActivity(intent)
+                            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                         } else if (value == "Seller") {
                             Log.d("User Logged", "User Logged in is Seller")
                             userType = "Seller"
@@ -45,6 +64,7 @@ class MainActivity : AppCompatActivity() {
                             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                             intent.putExtra("LoggedInUserEmail",document.getString("email"))
                             startActivity(intent)
+                            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                         } else {
                             Log.d("User Logged", "User Logged in is User")
                             userType = "User"
@@ -52,6 +72,7 @@ class MainActivity : AppCompatActivity() {
                             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                             intent.putExtra("LoggedInUserEmail",document.getString("email"))
                             startActivity(intent)
+                            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                         }
 
                     } else {
