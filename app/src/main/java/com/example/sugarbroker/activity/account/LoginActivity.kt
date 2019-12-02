f9f1f7293ecfaf47038813de2810f8cedaf89167
hemakothari12 <hkoth2@unh.newhaven.edu>
2019-12-02 17:21:39 -0500

    Login progress bar

--- app/src/main/java/com/example/sugarbroker/activity/account/LoginActivity.kt
+++ app/src/main/java/com/example/sugarbroker/activity/account/LoginActivity.kt
@@ -3,8 +3,11 @@ package com.example.sugarbroker.activity.account
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.text.Html
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.example.sugarbroker.R
 import com.example.sugarbroker.activity.home.AdminHomeActivity
@@ -14,17 +17,31 @@ import com.example.sugarbroker.activity.userType
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var i = 0
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        progressBar = findViewById<ProgressBar>(R.id.progress_Bar) as ProgressBar
+        progressBar!!.visibility = View.GONE
+
         val createAccount = "Don't have an account? " + "<font><b>" + "Sign up" + "<b></font>"
         newuser_textview.setText(Html.fromHtml(createAccount))
 
         login_button.setOnClickListener {
+            progressBar!!.visibility = View.VISIBLE
+            login_email_edittext.visibility = View.GONE
+            login_password_edittext.visibility = View.GONE
+            login_button.visibility = View.GONE
+            forgotpassword_textview.visibility = View.GONE
+            newuser_textview.visibility = View.GONE
             performLogin()
         }
 
@@ -65,12 +82,14 @@ class LoginActivity : AppCompatActivity() {
                             if (value == "Admin") {
                                 Log.d("User Logged", "User Logged in is Admin")
                                 userType = "Admin"
+                                progressBar!!.visibility = View.GONE
                                 intent = Intent(applicationContext, AdminHomeActivity::class.java)
                                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 startActivity(intent)
                             } else if (value == "Seller") {
                                 Log.d("User Logged", "User Logged in is Seller")
                                 userType = "Seller"
+                                progressBar!!.visibility = View.GONE
                                 intent = Intent(applicationContext, SellerHomeActivity::class.java)
                                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 intent.putExtra("LoggedInUserEmail",login_email_edittext.text.toString())
@@ -78,6 +97,7 @@ class LoginActivity : AppCompatActivity() {
                             } else {
                                 Log.d("User Logged", "User Logged in is User")
                                 userType = "User"
+                                progressBar!!.visibility = View.GONE
                                 intent = Intent(applicationContext, UserHomeActivity::class.java)
                                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 intent.putExtra("LoggedInUserEmail",login_email_edittext.text.toString())
